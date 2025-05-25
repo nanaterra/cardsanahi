2390b1da62f331bebaace8f840d1b03a093c0861
nanaterra <anahi.guterres@escola.pr.gov.br>
2025-05-25 20:35:55 +0000

    aulas-04-05-06-07-08

---
+++ app.js
@@ -0,0 +1,18 @@
+function criaCartao( categoria, pergunta, resposta){
+    let container= document.getElementById('container')
+    let cartao=document.createElement('article')
+    cartao.classname= 'cartao'
+
+    cartao.innerHTML= ` 
+      <div class="cartao_conteudo">
+     <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+    </div>
+     <div class="cartao_conteudo_reposta">
+     <p>${resposta}</p>
+    </div>
+    </div>
+     `
+     container.appendChild(cartao)
+}       
---
+++ assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que é a Tailândia?',
+    'É um país'
+)
+
+CriaCartao(
+    'Geografia,'
+    'Qual a Capital da Tailândia?',
+    'A Capital da Tailândia é Bangkok'
+)
+
+CriaCartao(
+    'Geografia',
+    'Qual é o tipo de governo na Tailândia?',
+    'A Tailândia é uma monarquia constitucional'
+)
+
+CriaCartao(
+    'Geografia',
+    'Como é composto o alfabeto tailandês?',
+    'É composto por 44 consoantes e 15 vogais'
+)
